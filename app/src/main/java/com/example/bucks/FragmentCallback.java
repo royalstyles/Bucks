7d8e56dabde38bf6ae3b23b8f50439b6414bed17
Jungho <royalstyles@naver.com>
2020-04-30 17:40:15 +0900

    nav 적용

---
+++ app/src/main/java/com/example/bucks/FragmentCallback.java
@@ -0,0 +1,9 @@
+package com.example.bucks;
+
+import android.os.Bundle;
+
+public interface FragmentCallback {
+
+    public void onFragmentSelected(int position, Bundle bundle);
+
+}
--- app/src/main/java/com/example/bucks/MainActivity.java
+++ app/src/main/java/com/example/bucks/MainActivity.java
@@ -1,59 +1,101 @@
 package com.example.bucks;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.drawerlayout.widget.DrawerLayout;
 
 
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageButton;
+import android.view.MenuItem;
+import android.widget.Toast;
+import android.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 
-    private DrawerLayout mainLayout;
-    private View drawerView;
-    private View mainView;
-    private ImageButton main_btnimg_list;
+public class MainActivity extends AppCompatActivity
+        implements FragmentCallback {
 
+    Fragment1 fragment1;
+    Fragment2 fragment2;
+    Fragment3 fragment3;
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        mainLayout = (DrawerLayout)findViewById(R.id.main_layout);
-        drawerView = (View)findViewById(R.id.drawerView);
-        mainView = (View)findViewById(R.id.main_view);
-        main_btnimg_list = (ImageButton)findViewById(R.id.main_btnimg_list);
-
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        main_btnimg_list.setOnClickListener(new View.OnClickListener(){
-//            @Override
-//            public void onClick(View v) {
-//                mainLayout.openDrawer(drawerView);
-//            }
-//        });
+        toolbar = findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+//        ActionBarDrawerToggle toggle  = new ActionBarDrawerToggle(
+//                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+//        drawer.addDrawerListener(toggle);
+//        toggle.syncState();
+//
+//        NavigationView navigationView = findViewById(R.id.nav_view);
+//        navigationView.setNavigationItemSelectedListener(this);
+
+        fragment1 = new Fragment1();
+        fragment2 = new Fragment2();
+        fragment3 = new Fragment3();
+
+        // 처음에는 fragment1 을 화면에 띄운다.
+        getSupportFragmentManager().beginTransaction().add(R.id.container, fragment1).commit();
+
     }
 
-    DrawerLayout.DrawerListener listener = new DrawerLayout.DrawerListener() {
-        @Override
-        public void onDrawerSlide(@NonNull View drawerView, float slideOffset) {
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+//    @Override
+//    public boolean onNavigationItemSelected(MenuItem item) {
+//        int id = item.getItemId();
+//
+//        if (id == R.id.menu1) {
+//            Toast.makeText(this, "첫번째 메뉴 선택됨.", Toast.LENGTH_LONG).show();
+//            onFragmentSelected(0, null);
+//        } else if (id == R.id.menu2) {
+//            Toast.makeText(this, "두번째 메뉴 선택됨.", Toast.LENGTH_LONG).show();
+//            onFragmentSelected(1, null);
+//        } else if (id == R.id.menu3) {
+//            Toast.makeText(this, "세번째 메뉴 선택됨.", Toast.LENGTH_LONG).show();
+//            onFragmentSelected(2, null);
+//        }
+//
+//        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+//        drawer.closeDrawer(GravityCompat.START);
+//        return true;
+//    }
+
+    @Override
+    public void onFragmentSelected(int position, Bundle bundle) {
+        Fragment curFragment = null;
+
+        if (position == 0) {
+            curFragment = fragment1;
+            toolbar.setTitle("첫번째 화면");
+        } else if (position == 1) {
+            curFragment = fragment2;
+            toolbar.setTitle("두번째 화면");
+        } else if (position == 2) {
+            curFragment = fragment3;
+            toolbar.setTitle("세번째 화면");
         }
 
-        @Override
-        public void onDrawerOpened(@NonNull View drawerView) {
-        }
-
-        @Override
-        public void onDrawerClosed(@NonNull View drawerView) {
-        }
-
-        @Override
-        public void onDrawerStateChanged(int newState) {
-        }
-    };
-
+        // curFragment 화면으로 변경시킨다.
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, curFragment).commit();
+    }
 
 }
